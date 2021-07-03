64a69b846e0c8a80643b266b5f34028eeb7f427e
Kanin Tansirisittikul <tan.kanin98@gmail.com>
2021-07-04 00:09:27 +0700

    add css to login page

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Calendar2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.assets.precompile += %w( sessions.js )
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
